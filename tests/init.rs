7236884f7d76a395264f6c861649a645fd29720f
Word30210 <wordisnottoto@gmail.com>
2026-08-17 22:33:48 +0900

    test: add some test cases

---
+++ tests/init.rs
@@ -0,0 +1,42 @@
+use assert_cmd::Command;
+use assert_fs::{TempDir, prelude::*};
+use predicates::prelude::*;
+
+#[test]
+fn init_creates_skeleton() {
+    let temp = TempDir::new().unwrap();
+
+    Command::cargo_bin("plumbum")
+        .unwrap()
+        .current_dir(&temp)
+        .args(["init", "myproj", "--scope", "acme"])
+        .assert()
+        .success();
+
+    temp.child("plumbum.toml")
+        .assert(predicate::path::is_file());
+    temp.child(".plumbum/configs/places")
+        .assert(predicate::path::is_dir());
+    temp.child(".plumbum/configs/packages")
+        .assert(predicate::path::is_dir());
+    temp.child(".plumbum/configs/scripts")
+        .assert(predicate::path::is_dir());
+    temp.child("places").assert(predicate::path::is_dir());
+    temp.child("packages").assert(predicate::path::is_dir());
+    temp.child("scripts").assert(predicate::path::is_dir());
+    temp.child("plumbum.toml")
+        .assert(predicate::str::contains("name = \"myproj\""));
+}
+
+#[test]
+fn init_fails_in_a_plumbum_project() {
+    let temp = TempDir::new().unwrap();
+    temp.child("plumbum.toml").write_str("hi").unwrap();
+
+    Command::cargo_bin("plumbum")
+        .unwrap()
+        .current_dir(&temp)
+        .arg("init")
+        .assert()
+        .failure();
+}
